eae1f5bc204bb74ee594ac0c119ab1bbc58e866b
Omar Shahbaz <omarshahbaz7@gmail.com>
2023-06-23 00:36:54 +0500

    PM-2 Add User Accounts

--- src/main/java/com/vault/passwordmanager/controller/AccountController.java
+++ src/main/java/com/vault/passwordmanager/controller/AccountController.java
@@ -1,14 +1,21 @@
 package com.vault.passwordmanager.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vault.passwordmanager.model.Account;
+import com.vault.passwordmanager.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/account")
 public class AccountController {
-
+    @Autowired
+    AccountService accountService;
     @GetMapping("/message")
     String display(){
         return "Hello Java!";
+    }
+    @PostMapping("/add")
+    Account addAccount(@RequestBody Account account){
+        return accountService.addAccount(account);
     }
 }
--- src/main/java/com/vault/passwordmanager/service/AccountService.java
+++ src/main/java/com/vault/passwordmanager/service/AccountService.java
@@ -1,4 +1,8 @@
 package com.vault.passwordmanager.service;
 
+import com.vault.passwordmanager.model.Account;
+
 public interface AccountService {
+
+    Account addAccount(Account account);
 }

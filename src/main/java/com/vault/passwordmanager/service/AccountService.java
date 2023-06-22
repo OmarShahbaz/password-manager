bbae48cbb52e9bc14d623906b6662b39b419785b
Omar Shahbaz <omarshahbaz7@gmail.com>
2023-06-23 01:19:05 +0500

    PM-3 Update User Account

--- src/main/java/com/vault/passwordmanager/service/AccountService.java
+++ src/main/java/com/vault/passwordmanager/service/AccountService.java
@@ -3,6 +3,6 @@ package com.vault.passwordmanager.service;
 import com.vault.passwordmanager.model.Account;
 
 public interface AccountService {
-
     Account addAccount(Account account);
+    Account updateAccount(int id, Account newAccount);
 }
